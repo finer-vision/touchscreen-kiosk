d952f655fdc449468cdeb475c99becd38c534536
Enijar <TheEnijar@gmail.com>
2023-09-13 10:23:56 +0100

    chore: fix 22.04 node.js install

--- bin/install-22.04.sh
+++ bin/install-22.04.sh
@@ -10,9 +10,10 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 
 NODE_MAJOR=18
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo dpkg -i --force-overwrite /var/cache/apt/archives/nodejs_$NODE_MAJOR.*-deb-1nodesource1_amd64.deb
 
 sudo apt update -y
-sudo apt-get install nodejs -y
+sudo apt install nodejs -y
 
 sudo ubuntu-drivers autoinstall
 curl https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
