7361ca06dbcacf6da4167458a6e76cf05fe5af12
Enijar <TheEnijar@gmail.com>
2023-09-13 10:09:57 +0100

    chore: update node.js install to the recommended way

--- bin/install-22.04.sh
+++ bin/install-22.04.sh
@@ -2,7 +2,18 @@
 
 printf "Setting up system...\n"
 sudo apt update -y
-sudo apt install -y dbus-x11 build-essential git nodejs unclutter xinput-calibrator chrome-gnome-shell libminizip1 libxcb-xinerama0 curl wget gnome-shell-extension-prefs
+sudo apt install -y ca-certificates gnupg dbus-x11 build-essential git unclutter xinput-calibrator chrome-gnome-shell libminizip1 libxcb-xinerama0 wget gnome-shell-extension-prefs
+
+# Node JS setup
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt update -y
+sudo apt-get install nodejs -y
+
 sudo ubuntu-drivers autoinstall
 curl https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 sudo dpkg -i teamviewer_*.deb
