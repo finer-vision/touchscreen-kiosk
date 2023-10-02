eb39aef198ffbd71a3c3efcc1f0d4e8cfb178747
James <TheEnijar@gmail.com>
2023-10-02 09:04:06 +0100

    fix: node 18 setup

--- bin/install-20.04.sh
+++ bin/install-20.04.sh
@@ -5,11 +5,13 @@ curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt update -y
 sudo apt install -y dbus-x11 build-essential git unclutter xinput-calibrator chrome-gnome-shell libminizip1 libxcb-xinerama0 curl wget gnome-shell-extension-prefs
 
+# Node JS setup
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
 NODE_MAJOR=18
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo dpkg -i --force-overwrite /var/cache/apt/archives/nodejs_$NODE_MAJOR.*-deb-1nodesource1_amd64.deb
-sudo apt update -y
-sudo apt install nodejs -y
 
 sudo ubuntu-drivers autoinstall
 curl https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
